abc649f209c9975fb26ec8fd945cda8a368233a8
izabella-m <izabella.pessoa03@outlook.com>
2025-01-14 04:13:57 -0300

    Adjust project

---
+++ Frontend/src/views/MainPage.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+      <h1>Hello world</h1>
+    </div>
+  </template>
+  
+  <script setup>
+  </script>
+  
+  <style scoped>
+
+  div {
+    text-align: center;
+  }
+  </style>
+  
