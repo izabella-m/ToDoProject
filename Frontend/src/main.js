869197f358daca0e964041b9d3d0c2b8ceab5213
izabella-m <izabella.pessoa03@outlook.com>
2025-01-16 23:44:44 -0300

    Importing the icon library

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -1,4 +1,17 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+// Importando a biblioteca FontAwesome Vue
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons'; // Importa todos os ícones sólidos
+
+// Adiciona todos os ícones da biblioteca 'fas' à biblioteca global
+library.add(fas);
+
+const app = createApp(App);
+
+// Registra o componente FontAwesomeIcon globalmente
+app.component('font-awesome-icon', FontAwesomeIcon);
+
+app.mount('#app');
